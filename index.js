2e0072ea8a012c479eff34059d90c6489b8b53be
AOsman0 <99136448+AOsman0@users.noreply.github.com>
2022-06-29 18:40:57 +0100

    latest changes

--- index.js
+++ index.js
@@ -5,12 +5,56 @@ const mysql = require("mysql2");
 
 // require inquirer
 const inquirer = require("inquirer");
+const createConnection = "../employee-tracker/db/schema.sql";
 
 // Hard code a connection
-
-const config = mysql.Connection({
+const db = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "password",
   database: "company_db",
 });
+
+db.connect((err) => {
+  if (err) throw err;
+  console.log(`Connected to the company_db database.`);
+});
+
+console.log(database);
+
+const initalQuestions = () =>
+  inquirer.prompt([
+    {
+      // prompt user with what they would like to do
+      type: "list",
+      name: "option ",
+      message: "Please select what you would like to do?",
+      choices: [
+        "view all departments",
+        "view all roles",
+        "view all employees",
+        "add a department",
+        "add a role",
+        "add an employee",
+        "update an employee role",
+        "quit",
+      ],
+    },
+  ]);
+// after there questions if the user selects view all departments
+
+// show the user the data from the schema file of all the departments
+
+// if the user selects view all roles
+// show the user all the roles in the db
+
+// if the user selects view all employees
+// show all employees from db
+
+// if user selects add department
+// prompt what department with you like to add
+// add the new department in the db
+
+// if user selects add role
+// prompt what role  with you like to add
+// add the new role  in the db
